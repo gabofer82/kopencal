8cc5bdb5f3f00e477d3fc90a5db0a4abbf152b7c
gabriel <gabofer82@gmail.com>
2014-02-14 20:50:08 -0200

    First test of engine. Not bad, not bad...

--- engine.py
+++ engine.py
@@ -4,35 +4,48 @@
 
 class CalcEngine(object):
 	def __init__(self):
-		self.equation = []
-		self.number_buffer = ''
+		self._operators = ['+', '-', '/', '*']
+		self._equation = []
+		self._number_buffer = ''
 
-	def addSimbol(self, symbol):
+	def addSymbol(self, symbol):
 		'''Add a simbol to ecuation'''
-		if self._evaluateSymbol(symbol) and len(self.equation) >= 1:
-			self.equation.append(self.number_buffer)
-			self.number_buffer = ''
-			self.equation.append(symbol)
+		if self._evaluateSymbol(symbol) and len(self._number_buffer) >= 1:
+			self._equation.append(self._number_buffer)
+			self._number_buffer = ''
+			self._equation.append(symbol)
 
 	def operate(self):
 		'''Return a result string. Invoqued by gui's equal button.'''
 		operation = ''
-		for item in self.equation:
+		print len(self._equation)
+		self._equation.append(self._number_buffer)
+		print self._equation
+		for item in self._equation:
 			operation = '%s%s' % (operation, item)
 
+		if self._isOperator(self._equation[-1]):
+			del self._equation[-1]
+
+		return eval(operation)
 
 	def cleanData(self):
 		'''Clean number_buffer variable.'''
-		self.number_buffer = ''
-		self.equation = []
+		self._number_buffer = ''
+		self._equation = []
 
 	def _evaluateSymbol(self, symbol):
-		if _isOperator():
+		if self._isOperator(symbol):
 			return True
 		else:
-			self.number_buffer = '%s%s' % (self.number_buffer, symbol,)
+			self._number_buffer = '%s%s' % (self._number_buffer, symbol,)
+
+		return False
 
 	def _isOperator(self, symbol):
 		'''Determine if symbol is operator or numeric digit.
 		Return True or False.'''
-		pass
+		if symbol in self._operators:
+			return True
+
+		return False
--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 #! /usr/bin/python
 
+import engine
+
 def initApp():
-	pass
+	eng = engine.CalcEngine()
+	eng.addSymbol('2')
+	eng.addSymbol('+')
+	eng.addSymbol('2')
+	eng.addSymbol('+')
+	eng.addSymbol('2')
+	eng.addSymbol('/')
+	eng.addSymbol('2')
+	print eng.operate()
 
 if __name__ == "__main__":
 	initApp()
